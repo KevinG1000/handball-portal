8feea15d93443c058df2e4de885b0862996f9efb
KevinG-Tech <gonzalezkevin443@gmail.com>
2025-02-10 12:31:07 -0500

    Initial commit - Set up MySQL database and Spring Boot configuration

---
+++ src/main/java/com/kevingonzalez/everythinghandball/EverythinghandballApplication.java
@@ -0,0 +1,13 @@
+package com.kevingonzalez.everythinghandball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythinghandballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverythinghandballApplication.class, args);
+	}
+
+}
