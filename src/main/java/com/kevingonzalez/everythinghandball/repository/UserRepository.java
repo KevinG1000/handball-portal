c9bd8d130ff675c47390d7f68d4e57f70d292c0b
KevinG-Tech <gonzalezkevin443@gmail.com>
2025-02-15 19:17:40 -0500

    Added findByUsername method in UserRepository for login functionality

--- src/main/java/com/kevingonzalez/everythinghandball/repository/UserRepository.java
+++ src/main/java/com/kevingonzalez/everythinghandball/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kevingonzalez.everythinghandball.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Optionally, add custom query methods
+    // Custom query method to find a user by username
     User findByUsername(String username);
 }
